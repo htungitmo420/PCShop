d3367c1b6e315723dc2faad6b19f0f09a63ab985
htungitmo420 <nguyentung23112005@gmail.com>
2025-11-04 02:44:10 +0300

    Add WebMvc configuration and JSP view support

--- src/main/java/vn/nhtw420/pcshop/controller/UserController.java
+++ src/main/java/vn/nhtw420/pcshop/controller/UserController.java
@@ -1,10 +1,11 @@
 package vn.nhtw420.pcshop.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vn.nhtw420.pcshop.service.UserService;
 
-@RestController
+@Controller
 public class UserController {
 
     private final UserService userService;
@@ -13,8 +14,23 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("")
+    @GetMapping("/")
     public String UserHello() {
-        return this.userService.handleHello();
+        return "hello";
     }
 }
+
+//@RestController
+//public class UserController {
+//
+//    private final UserService userService;
+//
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    @GetMapping("")
+//    public String UserHello() {
+//        return this.userService.handleHello();
+//    }
+//}
